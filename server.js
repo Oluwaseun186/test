2388a108aa6aa5e6bb0bd25d914393afa5cb4a78
oluwaseun186 <adewumibode7@gmail.com>
2024-11-09 01:02:37 +0100

    updated

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ mongoose.connect('mongodb://localhost:27017/todoapp', {
     useUnifiedTopology: true,
 });
 
+
 const taskSchema = new mongoose.Schema({
     title: String,
     completed: { type: Boolean, default: false },
